37d20c688bd880a110a3d17d901794ab7a67a019
Quentin DELETTRE <quentin.delettre@gmail.com>
2021-04-02 14:15:58 +0200

    feat(home): home page and theming

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -24,9 +24,9 @@ describe('HomeComponent', () => {
   });
 
   it('should have title', () => {
-    expect(fixture.nativeElement.querySelector('h1').textContent).toEqual(
-      'Awesome Hangman'
-    );
+    expect(
+      fixture.nativeElement.querySelector('h1.mat-display-1').textContent
+    ).toEqual('Awesome Hangman');
   });
 
   it('should have start button', () => {
