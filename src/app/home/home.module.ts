7433a0f1817ee4bfa90e3a0ffbb84f888b698c1e
Quentin DELETTRE <quentin.delettre@gmail.com>
2021-04-02 10:01:40 +0200

    wip

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutingModule, MatButtonModule],
+})
+export class HomeModule {}
