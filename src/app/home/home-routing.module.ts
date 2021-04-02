8bf819962c92a9e6bd6272a3230847a19c870d9c
Quentin DELETTRE <quentin.delettre@gmail.com>
2021-04-02 19:15:45 +0200

    feat(home): start a game

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'game',
+    loadChildren: () => import('./game/game.module').then((m) => m.GameModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
