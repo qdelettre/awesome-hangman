d36e86567569c946dfb4b61730b96aa5a320f69f
Quentin DELETTRE <quentin.delettre@gmail.com>
2021-04-12 22:49:52 +0200

    refactor: guards

---
+++ src/app/home/game/core/guards/win/win.guard.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  CanLoad,
+  Route,
+  UrlSegment,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import * as fromGame from '../../stores/game/game.selectors';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WinGuard implements CanActivate, CanLoad {
+  win$ = this.store
+    .select(fromGame.getWin)
+    .pipe(map((win) => (win ? true : this.router.createUrlTree(['/']))));
+
+  constructor(private store: Store, private router: Router) {}
+
+  canActivate(
+    _route: ActivatedRouteSnapshot,
+    _state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.win$;
+  }
+  canLoad(
+    _route: Route,
+    _segments: UrlSegment[]
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.win$;
+  }
+}
