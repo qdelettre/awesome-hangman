0962cac582c49c598b4b618524a5444a558f458a
Quentin Delettre <quentin.delettre@pm.me>
2022-01-23 18:19:29 +0100

    fix: unit tests coverage

--- src/app/home/game/core/stores/game/game.selectors.spec.ts
+++ src/app/home/game/core/stores/game/game.selectors.spec.ts
@@ -1,5 +1,6 @@
 import * as fromGame from './game.reducer';
 import {
+  getErrors,
   getGuessSorted,
   getLoose,
   getMaxErrorsReached,
@@ -15,23 +16,14 @@ describe('Game Selectors', () => {
   const chars = [char];
 
   it('should select the word', () => {
-    const result = getWord({
-      [fromGame.gameFeatureKey]: { ...fromGame.initialState, word },
-    });
+    const result = getWord.projector({ ...fromGame.initialState, word });
 
     expect(result).toEqual(word);
   });
 
   describe('sorted guess', () => {
-    const mockState = {
-      [fromGame.gameFeatureKey]: {
-        ...fromGame.initialState,
-        guess: [char, word],
-      },
-    };
-
     it('should select the GuessSorted', () => {
-      const result = getGuessSorted(mockState);
+      const result = getGuessSorted.projector([char, word]);
 
       expect(result).toEqual({
         chars,
@@ -41,27 +33,14 @@ describe('Game Selectors', () => {
   });
 
   describe('maxErrorsReached', () => {
-    const guess = [char, word];
-    it('should select false when reached', () => {
-      const result = getMaxErrorsReached({
-        [fromGame.gameFeatureKey]: {
-          ...fromGame.initialState,
-          guess,
-          rules: { maxErrors: 10 },
-        },
-      });
+    it('should select false when not reached', () => {
+      const result = getMaxErrorsReached.projector(1, 0);
 
       expect(result).toEqual(false);
     });
 
     it('should select true when reached', () => {
-      const result = getMaxErrorsReached({
-        [fromGame.gameFeatureKey]: {
-          ...fromGame.initialState,
-          guess,
-          rules: { maxErrors: 1 },
-        },
-      });
+      const result = getMaxErrorsReached.projector(1, 2);
 
       expect(result).toEqual(true);
     });
@@ -69,34 +48,24 @@ describe('Game Selectors', () => {
 
   describe('word chars', () => {
     it('should return the word when word is in guess', () => {
-      const result = getWordChars({
-        [fromGame.gameFeatureKey]: {
-          ...fromGame.initialState,
-          word,
-          guess: [word],
-        },
-      });
+      const result = getWordChars.projector(word, { chars: [], words: [word] });
 
       expect(result).toEqual([...word]);
     });
 
     it('should return the word when word is in guess as chars', () => {
-      const result = getWordChars({
-        [fromGame.gameFeatureKey]: {
-          ...fromGame.initialState,
-          word,
-          guess: [...word],
-        },
+      const result = getWordChars.projector(word, {
+        chars: [...word],
+        words: [],
       });
 
       expect(result).toEqual([...word]);
     });
 
     it('should return empty array when word is empty', () => {
-      const result = getWordChars({
-        [fromGame.gameFeatureKey]: {
-          ...fromGame.initialState,
-        },
+      const result = getWordChars.projector(fromGame.initialState.word, {
+        chars: [],
+        words: [],
       });
 
       expect(result).toEqual([]);
@@ -104,65 +73,51 @@ describe('Game Selectors', () => {
   });
 
   it('should return true when win', () => {
-    const result = getWin({
-      [fromGame.gameFeatureKey]: {
-        ...fromGame.initialState,
-        word,
-        guess: [word],
-      },
+    const result = getWin.projector(word, {
+      chars: [],
+      words: [word],
     });
 
     expect(result).toEqual(true);
   });
 
   it('should return false when not win', () => {
-    const result = getWin({
-      [fromGame.gameFeatureKey]: {
-        ...fromGame.initialState,
-        word,
-        guess: [char],
-      },
+    const result = getWin.projector(word, {
+      chars: [char],
+      words: [],
     });
 
     expect(result).toEqual(false);
   });
 
-  it('should return true when loose with char', () => {
-    const result = getLoose({
-      [fromGame.gameFeatureKey]: {
-        ...fromGame.initialState,
-        rules: { maxErrors: 1 },
-        word,
-        guess: ['x'],
-      },
-    });
+  it('should return true when max error reached', () => {
+    const result = getLoose.projector(false, true);
 
     expect(result).toEqual(true);
   });
 
-  it('should return true when loose with word', () => {
-    const result = getLoose({
-      [fromGame.gameFeatureKey]: {
-        ...fromGame.initialState,
-        rules: { maxErrors: 1 },
-        word,
-        guess: ['xx'],
-      },
-    });
-
-    expect(result).toEqual(true);
-  });
-
-  it('should return false when not loose', () => {
-    const result = getLoose({
-      [fromGame.gameFeatureKey]: {
-        ...fromGame.initialState,
-        rules: { maxErrors: 2 },
-        word,
-        guess: [char],
-      },
-    });
+  it('should return false when not max error reached', () => {
+    const result = getLoose.projector(false, false);
 
     expect(result).toEqual(false);
+  });
+
+  it('should return false when win', () => {
+    const result = getLoose.projector(true, false);
+
+    expect(result).toEqual(false);
+  });
+
+  it('should return errors', () => {
+    expect(getErrors.projector(word, { chars: [], words: [] })).toEqual(0);
+    expect(getErrors.projector(word, { chars: ['x'], words: [] })).toEqual(1);
+    expect(getErrors.projector(null, { chars: ['x'], words: [] })).toEqual(1);
+    expect(getErrors.projector(word, { chars: [], words: ['test'] })).toEqual(
+      1
+    );
+
+    expect(getErrors.projector(null, { chars: [], words: ['test'] })).toEqual(
+      1
+    );
   });
 });
--- src/app/home/game/core/stores/word/word.selectors.spec.ts
+++ src/app/home/game/core/stores/word/word.selectors.spec.ts
@@ -10,48 +10,34 @@ describe('Word Selectors', () => {
   const initialState = { ...fromWord.initialState };
 
   it('should select the feature state', () => {
-    const result = selectWordState({
-      [fromWord.wordFeatureKey]: initialState,
+    const result = selectWordState.projector({
+      ...initialState,
     });
 
     expect(result).toEqual(initialState);
   });
 
   it('should select the request state', () => {
-    const result = getRequestState({
-      [fromWord.wordFeatureKey]: initialState,
-    });
+    const result = getRequestState.projector({ ...initialState });
 
     expect(result).toEqual(initialState.requestState);
   });
 
   it('should select get words', () => {
-    const result = getWords({
-      [fromWord.wordFeatureKey]: initialState,
-    });
+    const result = getWords.projector({ ...initialState });
 
     expect(result).toEqual(initialState.words);
   });
 
   it('should get word when words', () => {
     const word = 'word';
-    const result = getWord(
-      {
-        [fromWord.wordFeatureKey]: { ...initialState, words: [word] },
-      },
-      { random: 0 }
-    );
+    const result = getWord.projector([word], { random: 0 });
 
     expect(result).toEqual(word);
   });
 
   it('should return null when words empty', () => {
-    const result = getWord(
-      {
-        [fromWord.wordFeatureKey]: { ...initialState, words: [] },
-      },
-      { random: 0 }
-    );
+    const result = getWord.projector([], { random: 0 });
 
     expect(result).toEqual(null);
   });
